59bb29230ea8568139b99d5f4ccc26fc7220cbfb
Alan <cf.alanpang@gmail.com>
2025-01-13 15:30:05 +0800

    add webhook

---
+++ app/routes/webhooks.app.order_created.tsx
@@ -0,0 +1,11 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }: any) => {
+  console.log(`Received order_created webhook`);
+  
+  const { shop, topic } = await authenticate.webhook(request);
+  console.log(`Received order_created webhook for ${shop}`);
+  console.log(`Received ${topic} webhook for ${shop}`);
+
+  return new Response();
+};
