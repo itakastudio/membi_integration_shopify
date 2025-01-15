7b8855947e3eb73a7a93cd5ce86be77a166170e6
Alan Chan Kit Pang <alan@AlanChan-Mac-mini.local>
2025-01-16 02:38:18 +0800

    add webhook

--- app/routes/webhooks.product_updated.tsx
+++ app/routes/webhooks.product_updated.tsx
@@ -3,9 +3,14 @@
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }: any) => {
+  console.log(`Received product_updated 1webhook`);
+  console.log(`Received product_updated 1webhook`);
+  console.log(`Received product_updated 1webhook`);
   console.log(`Received product_updated webhook`);
-  
+
+  // console.log(`Received product_updated webhook`, request);  
   const { shop, topic } = await authenticate.webhook(request);
+  console.log(`Received product_updated webhook for`);
   console.log(`Received product_updated webhook for ${shop}`);
   console.log(`Received ${topic} webhook for ${shop}`);
 
