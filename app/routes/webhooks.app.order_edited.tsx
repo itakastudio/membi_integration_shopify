d668104fb377507bcf56a515c1cd9bbdc7bc3cfb
Alan Chan Kit Pang <alan@AlanChan-Mac-mini.local>
2025-01-15 23:58:43 +0800

    edit console, edit scopes

--- app/routes/webhooks.app.order_edited.tsx
+++ app/routes/webhooks.app.order_edited.tsx
@@ -3,10 +3,10 @@
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }: any) => {
-  console.log(`Received order_created webhook`);
+  console.log(`Received order_edited webhook`);
   
   const { shop, topic } = await authenticate.webhook(request);
-  console.log(`Received order_created webhook for ${shop}`);
+  console.log(`Received order_edited webhook for ${shop}`);
   console.log(`Received ${topic} webhook for ${shop}`);
 
   return new Response();
