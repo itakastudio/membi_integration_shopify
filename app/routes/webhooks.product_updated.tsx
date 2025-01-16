246259520f88e7a24671360d277ad9e8f5c25f0d
Alan Chan Kit Pang <alan@AlanChan-Mac-mini.local>
2025-01-16 11:53:05 +0800

    add pg connection

--- app/routes/webhooks.product_updated.tsx
+++ app/routes/webhooks.product_updated.tsx
@@ -1,5 +1,6 @@
 // app/routes/webhooks.product_updated.tsx
 
+import pool from "app/pg.server";
 import { authenticate } from "../shopify.server";
 
 export const action = async ({ request }: any) => {
@@ -13,6 +14,14 @@ export const action = async ({ request }: any) => {
   console.log(`Received product_updated webhook for`);
   console.log(`Received product_updated webhook for ${shop}`);
   console.log(`Received ${topic} webhook for ${shop}`);
+
+  const client = await pool.connect()
+
+  const result = await client.query(
+    "SELECT * FROM system_tenant_login",
+  );
+
+  console.log(result.rows);
 
   return new Response();
 };
